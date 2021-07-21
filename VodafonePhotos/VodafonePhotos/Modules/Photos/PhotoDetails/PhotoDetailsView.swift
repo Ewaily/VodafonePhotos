a115824af8e268211c265597ce058eec2335350c
Muhammad Ewaily <muhammad.ewaily@gmail.com>
2021-07-21 08:48:29 +0200

    Implement zoom In/Out in details screen

--- VodafonePhotos/VodafonePhotos/Modules/Photos/PhotoDetails/PhotoDetailsView.swift
+++ VodafonePhotos/VodafonePhotos/Modules/Photos/PhotoDetails/PhotoDetailsView.swift
@@ -12,8 +12,9 @@ class PhotoDetailsView: BaseViewController {
     
     private var viewModel: PhotoDetailsViewModel!
     
-    @IBOutlet weak var authorNameLabel: UILabel!
+//    @IBOutlet weak var authorNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +27,8 @@ class PhotoDetailsView: BaseViewController {
     
     private func setupUI() {
         setupImageView()
-        authorNameLabel.text = viewModel.getAuthorName()
+        setupScrollView()
+        self.navigationItem.title = viewModel.getAuthorName()
         photoImageView.sd_imageIndicator = SDWebImageActivityIndicator.gray
         photoImageView.sd_setImage(with: viewModel.getPhotoDownloadURL(),
                                     placeholderImage: nil)
@@ -35,8 +37,46 @@ class PhotoDetailsView: BaseViewController {
     private func setupImageView() {
         photoImageView.cornerRadius = 8
         photoImageView.clipsToBounds = true
-        photoImageView.contentMode = .scaleAspectFill
+        photoImageView.contentMode = .scaleAspectFit
         photoImageView.sd_imageTransition = .fade
         photoImageView.sd_imageIndicator = SDWebImageActivityIndicator.gray
+    }
+    
+    private func setupScrollView() {
+        scrollView.maximumZoomScale = 4
+        scrollView.minimumZoomScale = 1
+        scrollView.delegate = self
+    }
+}
+
+// MARK: - UIScrollViewDelegate 
+
+extension PhotoDetailsView: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return photoImageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        if scrollView.zoomScale > 1 {
+            if let image = photoImageView.image {
+                let ratioW = photoImageView.frame.width / image.size.width
+                let ratioH = photoImageView.frame.height / image.size.height
+                
+                let ratio = ratioW < ratioH ? ratioW : ratioH
+                let newWidth = image.size.width * ratio
+                let newHeight = image.size.height * ratio
+                
+                let conditionLeft = newWidth * scrollView.zoomScale > photoImageView.frame.width
+                let left = 0.5 * (conditionLeft ? newWidth - photoImageView.frame.width : (scrollView.frame.width - scrollView.contentSize.width))
+                
+                let conditionTop = newHeight * scrollView.zoomScale > photoImageView.frame.height
+                let top = 0.5 * (conditionTop ? newHeight - photoImageView.frame.height : (scrollView.frame.height - scrollView.contentSize.height))
+                
+                scrollView.contentInset = UIEdgeInsets(top: top, left: left, bottom: top, right: left)
+            }
+        }
+        else {
+            scrollView.contentInset = .zero
+        }
     }
 }
